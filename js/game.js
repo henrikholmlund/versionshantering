885db403542f266d96812c865d58063dfdb905d2
Henrik Holmlund <henrik.holmlund@gmail.com>
2015-05-27 11:38:22 +0200

    bokstäver skrivs i resp ruta

--- js/game.js
+++ js/game.js
@@ -11,6 +11,13 @@ var button = document.getElementsByTagName("button")[0];
 button.addEventListener("click", function(){
 	var användarOrd = document.getElementsByTagName("input")[0].value;
 
+	//hämta rutor och lägg in bokstäverna rätt 
+	var rutor = document.getElementsByTagName("td");
+
+	for (var i = 0; i < 5; i++) {
+		rutor[i].innerHTML = användarOrd[i];
+	};
+
 	if (valtOrd === användarOrd) {
 		alert("GRattttisss!!");
 	} else {
